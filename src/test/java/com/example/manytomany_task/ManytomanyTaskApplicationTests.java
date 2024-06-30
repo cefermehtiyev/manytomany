c1a9fbb4be6228d31be1b155b3738f8b9decb5f1
Cefer Mehtiyev <cefer20051965@gmail.com>
2024-06-30 15:06:24 +0400

    first commit

---
+++ src/test/java/com/example/manytomany_task/ManytomanyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manytomany_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytomanyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
